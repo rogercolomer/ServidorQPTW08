99e3aede0ae077aed0e08696ffdffa4ac534601d
Roger <roger@QPTW08.quadpackgp.local>
2022-02-16 16:12:28 +0100

    Incorporacio del programa general main.sh

---
+++ main.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python3 /home/roger/repositori/biomassa/lecturaBacnet.py &
+python3 /home/roger/repositori/Telegram/botTelegram.py &
+python3 /home/roger/repositori/Telegram/alarmesProduccio.py &
+python3 /home/roger/repositori/Telegram/alarmesTelegram.py &
