297ecff69c13a828f2123679d263c0081a9c78d6
rogercolomer <83208646+rogercolomer@users.noreply.github.com>
2022-12-22 12:03:43 +0100

    Delete main.sh

--- main.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python3 /home/roger/repositori/ServidorQPWood/Biomassa/lecturaBacnet.py &
-#python3 /home/roger/repositori/ServidorQPWood/Telegram/botTelegram.py &
-#python3 /home/roger/repositori/ServidorQPWood/Telegram/alarmesProduccio.py &
-#python3 /home/roger/repositori/ServidorQPWood/Telegram/alarmesTelegram.py &
-python3 /home/roger/repositori/ServidorQPWood/execute.py &
