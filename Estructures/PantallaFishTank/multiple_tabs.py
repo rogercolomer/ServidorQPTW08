64619304722ec5c258321f70869ed1311e23e22e
rogercolomer <83208646+rogercolomer@users.noreply.github.com>
2022-12-22 12:01:57 +0100

    Update multiple_tabs.py

--- Estructures/PantallaFishTank/multiple_tabs.py
+++ Estructures/PantallaFishTank/multiple_tabs.py
@@ -21,7 +21,7 @@ chrome_options.add_experimental_option('prefs', {
 })
 
 driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options = chrome_options)
-driver.get('http:/147.45.44.203')
+driver.get('http://x.x.x.x')
 driver.maximize_window()
 
 userBiomas = (By.XPATH,"//input[@id='username']")
@@ -29,8 +29,8 @@ pwdBiomas = (By.XPATH,"//input[@id='password']")
 nextBiomas = (By.XPATH,"//body/div[1]/div[1]/form[1]/div[2]/input[1]")
 b1Biomas = (By.XPATH,"//body/div[@id='wrapper']/div[@id='main-area']/div[@id='submenu']/div[@id='submenu-tree']/ul[1]/li[2]/ul[1]/li[1]/a[1]/span[1]")
 b2Biomas = (By.XPATH,"//body/div[@id='wrapper']/div[@id='main-area']/div[@id='submenu']/div[@id='submenu-tree']/ul[1]/li[2]/ul[1]/li[1]/ul[1]/li[1]/a[1]/span[2]")
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable(userBiomas)).send_keys('produccio')
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable(pwdBiomas)).send_keys('Technotr@f!')
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable(userBiomas)).send_keys('xx')
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable(pwdBiomas)).send_keys('xx!')
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable(nextBiomas)).click()
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable(b1Biomas)).click()
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable(b2Biomas)).click()
@@ -43,8 +43,8 @@ driver.get('http://192.100.101.40:3000/d/cw6ECjTMk/aspiracio?orgId=1&refresh=1m'
 useGrafana = (By.XPATH, "//body/div[@id='reactRoot']/div[1]/div[2]/div[3]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]/input[1]")
 pwdGrafana = (By.XPATH, "//body/div[@id='reactRoot']/div[1]/div[2]/div[3]/div[1]/div[2]/div[1]/div[1]/form[1]/div[2]/div[2]/div[1]/div[1]/input[1]")
 nextGrafana = (By.XPATH, "//span[contains(text(),'Log in')]")
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable(useGrafana)).send_keys('fishTank')
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable(pwdGrafana)).send_keys('123456789')
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable(useGrafana)).send_keys('xxx')
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable(pwdGrafana)).send_keys('xx')
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable(nextGrafana)).click()
 time.sleep(5)
 #tercera tab compressors
@@ -91,20 +91,6 @@ while True:
     for g in genero:
         driver.switch_to.window(g)
         refresh()
-        # print(g,type(g))
-        # if pestanya == 1:
-        #     try:
-        #         WebDriverWait(driver, 20).until(EC.element_to_be_clickable(useGrafana)).send_keys('visualitzador')
-        #         WebDriverWait(driver, 20).until(EC.element_to_be_clickable(pwdGrafana)).send_keys('123456789')
-        #         WebDriverWait(driver, 20).until(EC.element_to_be_clickable(nextGrafana)).click()
-        #         WebDriverWait(driver, 20).until(EC.element_to_be_clickable(NEXT_BTN2)).click()
-        #         reset = 1
-        #     except:
-        #         pass
-        #     print('Ole')
-        # if reset == 1 and pestanya == 2:
-        #     pyautogui.press('f5')
-        #     reset =0
 
         time.sleep(30)
         pestanya += 1
